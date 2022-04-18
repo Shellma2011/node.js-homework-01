d8d3cb6f926a297b4d58b6f835e782fb78b4c244
Maryna Shevchenko <Shellma2011@gmail.com>
2022-04-18 12:34:27 +0300

    done

---
+++ README.md
@@ -0,0 +1,4 @@
+https://i.imgur.com/PLTvnxr.png
+https://i.imgur.com/HVEaUFd.png
+https://i.imgur.com/gAtZ4ub.png
+https://i.imgur.com/ibgK7Wz.png
---
+++ contacts.js
@@ -0,0 +1,47 @@
+const fs = require("fs/promises");
+const path = require("path");
+console.log(__dirname);
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+const { v4 } = require("uuid");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(id) {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === id);
+  if (!result) {
+    return null;
+  }
+  return result;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: v4(), name, email, phone };
+  await contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
+
+async function removeContact(id) {
+  const contacts = await listContacts();
+  const idx = await contacts.findIndex((item) => item.id === id);
+  if (idx === -1) {
+    return null;
+  }
+  const [removeContactById] = await contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return removeContactById;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
